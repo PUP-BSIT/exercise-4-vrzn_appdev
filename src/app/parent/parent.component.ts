87fdd8ca4c7f3cd4b4835f1c1c2d3b03077705a1
Jhean <drakekhendrick@gmail.com>
2024-04-29 20:59:35 +0800

    add needed components

---
+++ src/app/parent/parent.component.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { Student } from '../../model/student';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrl: './parent.component.css',
+})
+export class ParentComponent {
+  students: Student[] = [
+    { studentId: 0, givenName: 'Jhean', lastName: 'Galope', finalGrade: 1.25 },
+    { studentId: 1, givenName: 'Khen', lastName: 'Casane', finalGrade: 1.25 },
+  ];
+
+  newStudent: Student = {
+    studentId: null,
+    givenName: '',
+    lastName: '',
+    finalGrade: null,
+  };
+
+  addStudent() {
+    let studentId = this.students.length + 1;
+
+    this.newStudent = {
+      studentId: studentId,
+      givenName: this.newStudent.givenName,
+      lastName: this.newStudent.lastName,
+      finalGrade: this.newStudent.finalGrade,
+    };
+
+    this.students.push(this.newStudent);
+
+    this.newStudent = {
+      studentId: null,
+      givenName: '',
+      lastName: '',
+      finalGrade: null 
+	};
+  }
+
+  onUpdate(studentId: number) {
+    const studentToUpdate = this.students.findIndex(
+      (student) => student.studentId === studentId
+    );
+	this.newStudent = { ...this.students[studentToUpdate] };
+  }
+
+  updateStudent() {
+	this.students[this.newStudent.studentId] = { ...this.newStudent };
+
+	this.newStudent = {
+    	studentId: null,
+    	givenName: '',
+    	lastName: '',
+    	finalGrade: null,
+  	};
+  }
+
+  removeStudent(studentId: number) {
+    this.students.splice(studentId, 1);
+  }
+}
